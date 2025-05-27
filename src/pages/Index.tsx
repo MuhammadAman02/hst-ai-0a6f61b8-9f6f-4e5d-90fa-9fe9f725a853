d3d07cc20cc6b2ed691b11c09b9ccd2efe530095
MuhammadAman02 <mohammada@hst.ie>
2025-05-27 18:24:47 +0500

    update file

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Index = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
-      <h1 className="text-6xl font-bold text-white">Hello HST!</h1>
+      <h1 className="text-6xl font-bold text-white">Hello HST!!</h1>
     </div>
   );
 };
